8034b3cf03fcb17cdba6fad39ace2fbfad28e48c
4sizn <4sizn@naver.com>
2021-09-14 23:54:53 +0900

    feat : update types

--- src/broadcastChannel.ts
+++ src/broadcastChannel.ts
@@ -1,75 +1,75 @@
 import { EventEmitter } from "events";
 
 class Client {
-  private _subscriptions: {
-    [key: string]: BroadcastChannel;
-  };
-  private _emitter: EventEmitter;
-  constructor() {
-    this._subscriptions = {};
-    this._emitter = new EventEmitter();
-  }
+	private _subscriptions: {
+		[key: string]: BroadcastChannel;
+	};
+	private _emitter: EventEmitter;
+	constructor() {
+		this._subscriptions = {};
+		this._emitter = new EventEmitter();
+	}
 
-  isSupport() {
-    return "BroadcastChannel" in self;
-  }
-  subscribe(topic = "/") {
-    if (!this.isSupport()) {
-      throw new Error("BroadcastChannel API not supported!");
-    }
+	isSupport() {
+		return "BroadcastChannel" in self;
+	}
+	subscribe(topic = "/") {
+		if (!this.isSupport()) {
+			throw new Error("BroadcastChannel API not supported!");
+		}
 
-    if (this._subscriptions[topic]) {
-      return this._subscriptions[topic];
-    }
+		if (this._subscriptions[topic]) {
+			return this._subscriptions[topic];
+		}
 
-    this._subscriptions[topic] = new BroadcastChannel(topic);
-    this._subscriptions[topic].onmessage = this._handleOnMessage.bind(this);
-    return this._subscriptions[topic];
-  }
+		this._subscriptions[topic] = new BroadcastChannel(topic);
+		this._subscriptions[topic].onmessage = this._handleOnMessage.bind(this);
+		return this._subscriptions[topic];
+	}
 
-  _handleOnMessage(args) {
-    console.log("_handleOnMessage", args);
-    const { data } = args;
-    const { eventName, payload } = data;
-    this._emitter.emit(`${eventName}`, payload);
-  }
+	_handleOnMessage(args: { data: { eventName: string; payload: any } }) {
+		console.log("_handleOnMessage", args);
+		const { data } = args;
+		const { eventName, payload } = data;
+		this._emitter.emit(`${eventName}`, payload);
+	}
 
-  on(eventName, listener) {
-    this._emitter.on(eventName, listener);
-  }
+	on(eventName: string, listener: EventListener) {
+		this._emitter.on(eventName, listener);
+	}
 
-  once(eventName, listener) {
-    this._emitter.once(eventName, listener);
-  }
+	once(eventName: string, listener: EventListener) {
+		this._emitter.once(eventName, listener);
+	}
 
-  off(eventName, listener) {
-    this._emitter.off(eventName, listener);
-  }
+	off(eventName: string, listener: EventListener) {
+		this._emitter.off(eventName, listener);
+	}
 
-  isSubscribed(topic: string) {
-    return Object.keys(this._subscriptions).includes(topic);
-  }
+	isSubscribed(topic: string) {
+		return Object.keys(this._subscriptions).includes(topic);
+	}
 
-  send<T>(topic = "/", message: T) {
-    if (!this.isSupport()) {
-      throw new Error("BroadcastChannel API not supported!");
-    }
-    new BroadcastChannel(topic).postMessage(message);
-  }
+	send<T>(topic = "/", message: T) {
+		if (!this.isSupport()) {
+			throw new Error("BroadcastChannel API not supported!");
+		}
+		new BroadcastChannel(topic).postMessage(message);
+	}
 
-  publish(topic = "/", message) {
-    this.send(topic, message);
-  }
+	publish<T>(topic = "/", message: T) {
+		this.send(topic, message);
+	}
 
-  disconnect(topic: string) {
-    if (topic) {
-      this._subscriptions[topic].close();
-      delete this._subscriptions[topic];
-    } else {
-      Object.values(this._subscriptions).forEach((o) => o.close());
-      this._subscriptions = {};
-    }
-  }
+	disconnect(topic: string) {
+		if (topic) {
+			this._subscriptions[topic].close();
+			delete this._subscriptions[topic];
+		} else {
+			Object.values(this._subscriptions).forEach((o) => o.close());
+			this._subscriptions = {};
+		}
+	}
 }
 
 const broadCastChannel = new Client();
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,9 @@
-import './style.css'
+import "./style.css";
+import broadcast from "./broadcastChannel";
 
-const app = document.querySelector<HTMLDivElement>('#app')!
+const app = document.querySelector<HTMLDivElement>("#app")!;
 
 app.innerHTML = `
   <h1>Hello Vite!</h1>
   <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`
+`;
